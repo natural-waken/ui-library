e7e774e06ae3b5fab0eba439e8f05e6d34de6b64
liyitong <3199941412@qq.com>
2024-07-11 18:02:16 +0800

    commit

--- node_modules/ui-library/dist/types/components/Button/constants.d.ts
+++
@@ -1,4 +0,0 @@
-import { InjectionKey } from 'vue';
-import { ButtonGroupContext } from './types';
-
-export declare const BUTTON_GROUP_CTX_KEY: InjectionKey<ButtonGroupContext>;
--- node_modules/ui-library/dist/types/utils/install.d.ts
+++
@@ -1,6 +0,0 @@
-import { Plugin } from 'vue';
-
-type SFCWithInstall<T> = T & Plugin;
-export declare function makeInstaller(components: Plugin[]): Plugin;
-export declare const withInstall: <T>(component: T) => SFCWithInstall<T>;
-export {};
